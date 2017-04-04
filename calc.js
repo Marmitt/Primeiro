e157f6878acf3a5f346cf7d373015673582c5262
Marmitt <lmateus.marmitt@gmail.com>
2017-04-04 20:44:00 -0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,7 +1,14 @@
 'use strict';
 
-function soma(a, b) {
-  return a + b;
+function soma(a, b)
+{
+  if (a === undefined || b === undefined )
+    return { message: 'parâmentos faltando'};
+
+  if (typeof a == 'string' || typeof b == 'string')
+    return { message: 'parâmentos invalidos'};
+
+    return a + b;
 }
 
 module.exports = { soma };
