80c0dc328f4937266188832a65961d053a4337ff
Marmitt <lmateus.marmitt@gmail.com>
2017-04-04 20:43:34 -0300

    Update calc.spec.js

--- calc.spec.js
+++ calc.spec.js
@@ -2,6 +2,8 @@ const Code = require('code');
 const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 
+const { expect } = require('code');
+
 const { soma } = require('./calc');
 
 lab.test('retorno da soma de 2 e 3 deve ser 5', (done) => {
@@ -9,28 +11,29 @@ lab.test('retorno da soma de 2 e 3 deve ser 5', (done) => {
     Code.expect(soma(2, 3)).to.equal(5);
     done();
 });
-lab.test('soma de letras é igual a 0', (done) => {
-
-    Code.expect(soma(5,d)).to.equal(5);
-    done();
-});
-lab.test('soma de 5 número com nada é igual a 5', (done) => {
-
-    Code.expect(soma(5, )).to.equal(5);
-    done();
-});
 lab.test('retorno da soma de -5 e 6 deve ser 1', (done) => {
 
     Code.expect(soma(-5, 6)).to.equal(1);
     done();
 });
-lab.test('retorno da soma de -5 e -7 deve ser -12', (done) => {
+lab.test('retorno da soma de -5 e -7 deve ser 1', (done) => {
 
     Code.expect(soma(-5, -7)).to.equal(-12);
     done();
 });
-lab.test('retorno da soma de nada com nada deve ser nada', (done) => {
-
-    Code.expect(soma(,)).to.equal();
+lab.test('não passando parâmetros', (done) => {
+    const r = soma();
+    expect(r.message).to.equal("parâmentos faltando")
     done();
+});
+lab.test('Soma com 0', (done) => {
+
+    Code.expect(soma(0, 70)).to.equal(70);
+    done();
+});
+lab.test('Soma nada com nada', (done) => {
+  const r = soma('a','b');
+  expect(r.message).to.equal("parâmentos invalidos")
+  done();
+
 });
